19a176f7db1ff8bb7b047455852770d11a0d5474
Dhruv Agarwal <dhruvagga@gmail.com>
2014-05-17 04:25:00 +0530

    modified main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ gs=goslate.Goslate()
 if len(sys.argv)==1:
 	s=raw_input('Enter the text to be translated: ')
 else:
-	s=' '.join(sys.argv[1:])
+	s='\n'.join(sys.argv[1:])
 print gs.translate(s,'en')
 
