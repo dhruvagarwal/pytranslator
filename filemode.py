5d240766289425451f5596bb3c36d5d5e5d517b8
Dhruv Agarwal <dhruvagga@gmail.com>
2014-05-17 05:43:04 +0530

    filemode translation

---
+++ filemode.py
@@ -0,0 +1,4 @@
+import goslate,sys
+gs=goslate.Goslate()
+while True:
+	print gs.translate(raw_input(),'en') 
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ gs=goslate.Goslate()
 if len(sys.argv)==1:
 	s=raw_input('Enter the text to be translated: ')
 else:
-	s='\n'.join(sys.argv[1:])
+	s=' '.join(sys.argv[1:])
 print gs.translate(s,'en') #set your default language here
